1be89eae2256cd89d9b58877bedaf606829df7c3
Jessy BREYNE <jessybreyne@protonmail.com>
2021-05-27 14:34:02 +0200

    Découverte de react-navigation

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
+
+import WelComeScreen from './screens/WelcomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Welcome" component={WelComeScreen}/>
+        <Screen name="Login" component={LoginScreen}/>
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
